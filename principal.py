165cade446d94c73205d40dff748a22bccec19cb
Rafael Dadoorian <30996645+rdpreg@users.noreply.github.com>
2025-04-18 06:58:00 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 # Inicializa o menu ativo se não existir
 if "menu" not in st.session_state:
-    st.session_state.menu = "Ferramenta 1"
+    st.session_state.menu = "Relatório diário de PL"
 
 # Botões de menu
-if st.sidebar.button("Ferramenta 1"):
-    st.session_state.menu = "Ferramenta 1"
+if st.sidebar.button("Relatório diário de PL"):
+    st.session_state.menu = "Relatório diário de PL"
 
 if st.sidebar.button("Ferramenta 2"):
     st.session_state.menu = "Ferramenta 2"
@@ -15,7 +15,7 @@ if st.sidebar.button("Ferramenta 3"):
     st.session_state.menu = "Ferramenta 3"
 
 # Conteúdo da página principal
-st.title("Meu Web App")
+st.title("Bem-vindo(a) a área de relatórios")
 
 if st.session_state.menu == "Ferramenta 1":
     st.header("Ferramenta 1")
