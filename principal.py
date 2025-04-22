db1f57ff9ef9c92fadbf45c18cda29b5be2f13ae
Rafael Dadoorian <30996645+rdpreg@users.noreply.github.com>
2025-04-22 05:40:09 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from ferramentas import relatorio_diario
 from ferramentas import aviso_rf
+from ferramentas import nnm-diario
 
 # Inicializa o menu padrão
 if "menu" not in st.session_state:
